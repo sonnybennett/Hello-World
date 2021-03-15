d79ffd10ae7de48de6031576f97117077dc3ee6d
Sonny Bennett <RC0042859@student.riversidecollege.ac.uk>
2021-03-15 15:24:28 +0000

    added hello button

--- Hello World/Form1.cs
+++ Hello World/Form1.cs
@@ -24,7 +24,7 @@ namespace Hello_World
 
         private void Button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Hello_World");
+            MessageBox.Show("Hello" + txtBxName.Text);
         }
     }
 }
